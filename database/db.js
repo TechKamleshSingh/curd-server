2a8e3ff142f9cb5dba0f5266d839ee467d11b087
TechKamleshSingh <justkamleshsingh@gmail.com>
2024-04-06 17:00:41 +0530

    Added the live db

--- database/db.js
+++ database/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose"
 
 const Connection = async (username, password) => {
-    const URL = `mongodb+srv://${username}:${password}@crud-app.hbv6j.mongodb.net/?retryWrites=true&w=majority`;
+    // const URL = `mongodb+srv://${username}:${password}@crud-app.hbv6j.mongodb.net/?retryWrites=true&w=majority`;
+    const URL = `mongodb+srv://${username}:${password}@cluster0.l6fljrr.mongodb.net/curd-management?retryWrites=true&w=majority&appName=Cluster0`;
                
     try {
         await mongoose.connect(URL)
