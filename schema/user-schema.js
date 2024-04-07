2e3a4f3f624956278490887c6ac50bf4b09d1019
TechKamleshSingh <justkamleshsingh@gmail.com>
2024-04-07 05:32:13 +0530

    Changes

--- schema/user-schema.js
+++ schema/user-schema.js
@@ -1,17 +1,21 @@
 import mongoose from "mongoose";
-import autoIncrement from 'mongoose-auto-increment'
+// import autoIncrement from 'mongoose-auto-increment'
 
+const userSchema = mongoose.Schema(
+  {
+    name: String,
+    user_name: String,
+    email: String,
+    phone: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const userSchema = mongoose.Schema({
-    name : String,
-    user_name : String,
-    email : String,
-    phone : String
-})
+// autoIncrement.initialize(mongoose.connection)
+// userSchema.plugin(autoIncrement.plugin, 'user')
 
-autoIncrement.initialize(mongoose.connection)
-userSchema.plugin(autoIncrement.plugin, 'user')
+const userModel = mongoose.model("user", userSchema);
 
-const userModel = mongoose.model('user', userSchema)
-
-export default userModel
+export default userModel;
